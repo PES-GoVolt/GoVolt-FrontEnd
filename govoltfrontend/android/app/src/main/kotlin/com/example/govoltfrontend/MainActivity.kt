fa67798ec55fbfa5fece97015ccacf16f71a808b
evendrell <esther.vendrell@estudiantat.upc.edu>
2023-10-09 18:30:24 +0200

    iniciar proyecto front

---
+++ govoltfrontend/android/app/src/main/kotlin/com/example/govoltfrontend/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.govoltfrontend
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ govoltfrontend/lib/config.dart
@@ -0,0 +1,8 @@
+class Config {
+  static const String appName = "Go Volt";
+  static const String apiURL = '192.168.122.1'; // api de cada ordenador
+  static const productosAPI = "api/producto";
+  static const clientesAPI = "api/cliente";
+  static const loginAPI = "api/login/";
+  static const obtenertokenAPI = "api/api-token-auth/";
+}
