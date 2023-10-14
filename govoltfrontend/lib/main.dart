41c75ea0ddae85a88082c96632371f0b9123269e
Marc Castro Chavez <104657474+MarcChavez@users.noreply.github.com>
2023-10-14 19:51:09 +0200

    map done

--- govoltfrontend/lib/main.dart
+++ govoltfrontend/lib/main.dart
@@ -1,191 +1,44 @@
-import 'dart:convert';
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
-import 'package:govoltfrontend/usuario.dart';
-import 'package:govoltfrontend/menu.dart';
-import 'package:govoltfrontend/pages/cliente/cliente_add_edit.dart';
-import 'package:govoltfrontend/pages/cliente/cliente_list.dart';
-import 'package:govoltfrontend/pages/producto/producto_add_edit.dart';
-import 'package:govoltfrontend/pages/producto/producto_list.dart';
-import 'package:govoltfrontend/config.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 void main() => runApp(const MyApp());
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
 
-  static const String _title = Config.appName;
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  late GoogleMapController mapController;
+
+  final LatLng _center = const LatLng(45.521563, -122.677433);
+
+  void _onMapCreated(GoogleMapController controller) {
+    mapController = controller;
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: _title,
-        home: Scaffold(
-          appBar: AppBar(title: const Text(_title)),
-          body: const MyStatefulWidget(),
+      theme: ThemeData(
+        useMaterial3: true,
+        colorSchemeSeed: Colors.green[700],
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Maps Sample App'),
+          elevation: 2,
         ),
-        routes: {
-          '/list-cliente': (context) => const ClientesList(),
-          '/add-cliente': (context) => const ClienteAddEdit(),
-          '/edit-cliente': (context) => const ClienteAddEdit(),
-          '/list-producto': (context) => const ProductosList(),
-          '/add-producto': (context) => const ProductoAddEdit(),
-          '/edit-producto': (context) => const ProductoAddEdit(),
-          '/home': (context) => Menu(),
-        });
-  }
-}
-
-class MyStatefulWidget extends StatefulWidget {
-  const MyStatefulWidget({Key? key}) : super(key: key);
-
-  @override
-  State<MyStatefulWidget> createState() => _MyStatefulWidgetState();
-}
-
-class _MyStatefulWidgetState extends State<MyStatefulWidget> {
-  TextEditingController nameController = TextEditingController();
-  TextEditingController passwordController = TextEditingController();
-
-  //final urllogin = Uri.parse("http://192.168.1.108/api/login/");
-  final urllogin = Uri.http(Config.apiURL, Config.loginAPI);
-
-  //final urlobtenertoken = Uri.parse("http://192.168.1.108/api/api-token-auth/");
-  final urlobtenertoken = Uri.http(Config.apiURL, Config.obtenertokenAPI);
-  final headers = {"Content-Type": "application/json;charset=UTF-8"};
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-        padding: const EdgeInsets.all(10),
-        child: ListView(
-          children: <Widget>[
-            Container(
-                alignment: Alignment.center,
-                padding: const EdgeInsets.all(10),
-                child: const Text(
-                  Config.appName,
-                  style: TextStyle(
-                      color: Colors.blue,
-                      fontWeight: FontWeight.w500,
-                      fontSize: 30),
-                )),
-            Container(
-                alignment: Alignment.center,
-                padding: const EdgeInsets.all(10),
-                child: const Text(
-                  'Ingresar',
-                  style: TextStyle(fontSize: 20),
-                )),
-            Container(
-              padding: const EdgeInsets.all(10),
-              child: TextField(
-                controller: nameController,
-                decoration: const InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: 'User Name',
-                ),
-              ),
-            ),
-            Container(
-              padding: const EdgeInsets.fromLTRB(10, 10, 10, 0),
-              child: TextField(
-                obscureText: true,
-                controller: passwordController,
-                decoration: const InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: 'Password',
-                ),
-              ),
-            ),
-            TextButton(
-              onPressed: () {
-                //forgot password screen
-              },
-              child: const Text(
-                'Olvido su contraseña',
-              ),
-            ),
-            Container(
-                height: 50,
-                padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
-                child: ElevatedButton(
-                  child: const Text('Acceder'),
-                  onPressed: () {
-                    login();
-                  },
-                )),
-            Row(
-              // ignore: sort_child_properties_last
-              children: <Widget>[
-                const Text('No tiene Cuenta ?'),
-                TextButton(
-                  child: const Text(
-                    'Registrarse',
-                    style: TextStyle(fontSize: 20),
-                  ),
-                  onPressed: () {
-                    //signup screen
-                  },
-                )
-              ],
-              mainAxisAlignment: MainAxisAlignment.center,
-            ),
-          ],
-        ));
-  }
-
-  void showSnackbar(String msg) {
-    final snack = SnackBar(content: Text(msg));
-    ScaffoldMessenger.of(context).showSnackBar(snack);
-  }
-
-  Future<void> login() async {
-    if (nameController.text.isEmpty || passwordController.text.isEmpty) {
-      showSnackbar(
-          "${nameController.text.isEmpty ? "-User " : ""} ${passwordController.text.isEmpty ? "- Contraseña " : ""} requerido");
-      return;
-    }
-    final datosdelposibleusuario = {
-      "username": nameController.text,
-      "password": passwordController.text
-    };
-    final res = await http.post(urllogin,
-        headers: headers, body: jsonEncode(datosdelposibleusuario));
-    //final data = Map.from(jsonDecode(res.body));
-    print(res.statusCode);
-    if (res.statusCode == 400) {
-      showSnackbar("error");
-      return;
-    }
-    if (res.statusCode != 200) {
-      showSnackbar("Ups ha habido un al obtener usuario y contraseña ");
-    }
-    final res2 = await http.post(urlobtenertoken,
-        headers: headers, body: jsonEncode(datosdelposibleusuario));
-    print("aqui 1");
-    final data2 = Map.from(jsonDecode(res2.body));
-    print("aqui 2");
-    if (res2.statusCode == 400) {
-      showSnackbar("error");
-      return;
-    }
-    if (res2.statusCode != 200) {
-      showSnackbar("Ups ha habido al obtener el token ");
-    }
-    final token = data2["token"];
-    final user = Usuario(
-        username: nameController.text,
-        password: passwordController.text,
-        token: token);
-    print("aqui si");
-    // ignore: use_build_context_synchronously
-    //Navigator.push(context,MaterialPageRoute(builder: (context) => Home()),);
-    // ignore: use_build_context_synchronously
-    Navigator.pushNamed(
-      context,
-      '/home',
+        body: GoogleMap(
+          onMapCreated: _onMapCreated,
+          initialCameraPosition: CameraPosition(
+            target: _center,
+            zoom: 11.0,
+          ),
+        ),
+      ),
     );
   }
 }
