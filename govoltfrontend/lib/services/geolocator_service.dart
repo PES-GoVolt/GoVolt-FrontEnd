59e5d087b76799f4037e75ea78a624217fc5a3db
Marc Castro Chavez <104657474+MarcChavez@users.noreply.github.com>
2023-10-20 19:13:49 +0200

    agresivos

--- govoltfrontend/lib/services/geolocator_service.dart
+++ govoltfrontend/lib/services/geolocator_service.dart
@@ -1,21 +1,27 @@
+import 'dart:io';
+
 import 'package:geolocator/geolocator.dart';
 
 class GeolocatiorService {
   Stream<Position> getCurrentLocation() {
     var locationOptions = const LocationSettings(
         accuracy: LocationAccuracy.best, distanceFilter: 2);
+    askForPermissions();
     return Geolocator.getPositionStream(locationSettings: locationOptions);
   }
 
-  Future<Position> getInitialLocation() async {
+  Future<LocationPermission> askForPermissions() async {
     LocationPermission permission = await Geolocator.checkPermission();
-    if (permission == LocationPermission.denied) {
+    if (permission == LocationPermission.denied ||
+        permission == LocationPermission.deniedForever ||
+        permission == LocationPermission.unableToDetermine) {
       permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied ||
+          permission == LocationPermission.deniedForever) {
+        Geolocator.openLocationSettings();
+        exit(0);
+      }
     }
-    if (permission == LocationPermission.denied) {
-      // El usuario rechazó los permisos de ubicación, puedes manejarlo aquí.
-    }
-    return Geolocator.getCurrentPosition(
-        desiredAccuracy: LocationAccuracy.high);
+    return permission;
   }
 }
