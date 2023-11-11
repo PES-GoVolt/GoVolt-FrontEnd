423b7e366ffeac823f224e3ef28e2639cd7f5aca
rubenrivi <rubenrivilloldo@gmail.comn>
2023-11-11 20:57:20 +0100

    profile editing

--- govoltfrontend/lib/volter.dart
+++ govoltfrontend/lib/volter.dart
@@ -3,6 +3,8 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:govoltfrontend/config.dart';
+import 'package:image_picker/image_picker.dart';
+import 'dart:io';
 
 import 'dart:convert';
 
@@ -18,11 +20,64 @@ class _VolterScreenState extends State<VolterScreen> {
   TextEditingController lastNameController = TextEditingController();
   bool edit = false; // Estado para controlar si se está editando
 
+  XFile? _imageFile;
   String email = '';
   String phoneNumber = '';
   String firstName = '';
   String lastName = '';
-  String photo='';
+  String photo = '';
+
+
+  Future<void> _getImage() async {
+    final pickedFile = await ImagePicker().getImage(
+      source: ImageSource.gallery,
+    );
+
+    if (pickedFile != null) {
+      setState(() {
+        _imageFile = XFile(pickedFile.path);
+      });
+    }
+  }
+
+ 
+
+
+  void saveChanges() async {
+    try {
+      firstName = firstNameController.text;
+      lastName = lastNameController.text;
+      email = emailController.text;
+      phoneNumber = phoneNumberController.text;
+
+      Map<String, dynamic> requestBody = {
+        'first_name': firstName,
+        'last_name': lastName,
+        'phone': phoneNumber,
+        'photo_url': photo
+        // Otros campos...
+      };
+
+      final response = await http.post(
+        Uri.http(Config.apiURL, Config.editMyProfileAPI),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: jsonEncode(requestBody),
+      );
+
+      if (response.statusCode == 200) {
+        print("Cambios guardados exitosamente");
+      } else {
+        print(
+            "Error al guardar los cambios. Código de estado: ${response.statusCode}");
+        // Puedes manejar el error de acuerdo a tus necesidades
+      }
+    } catch (error) {
+      print("Error al realizar la solicitud HTTP: $error");
+      // Puedes manejar el error de acuerdo a tus necesidades
+    }
+  }
 
   Future<void> fetchProfileData() async {
     final response =
@@ -94,25 +149,49 @@ class _VolterScreenState extends State<VolterScreen> {
             Row(
               mainAxisAlignment: MainAxisAlignment.start,
               children: [
-                CircleAvatar(
-                  radius: 50,
-                  backgroundColor: Colors.grey,
-                  child: Icon(
-                    Icons.person,
-                    size: 50,
-                    color: Colors.white,
-                  ),
+                Stack(
+                  alignment: Alignment.bottomRight,
+                  children: [
+                    CircleAvatar(
+                      radius: 50,
+                      backgroundColor: Colors.grey,
+                      child: _imageFile != null
+                          ? ClipOval(
+                              child: Image.file(
+                                File(_imageFile!.path),
+                                width: 100,
+                                height: 100,
+                                fit: BoxFit.cover,
+                              ),
+                            )
+                          : Icon(
+                              Icons.person,
+                              size: 50,
+                              color: Colors.white,
+                            ),
+                    ),
+                    if (edit)
+                      GestureDetector(
+                        onTap: () async {
+                          await _getImage();
+                        },
+                        child: CircleAvatar(
+                          radius: 20,
+                          backgroundColor: Colors.blue,
+                          child: Icon(
+                            Icons.edit,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                  ],
                 ),
                 SizedBox(width: 20),
                 Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
                     Text(
-                      firstName,
-                      style: TextStyle(fontSize: 16),
-                    ),
-                    Text(
-                      lastName,
+                      '$firstName $lastName',
                       style: TextStyle(fontSize: 16),
                     ),
                     Text(
@@ -136,6 +215,15 @@ class _VolterScreenState extends State<VolterScreen> {
                     setState(() {
                       // Cambia el estado "edit" al contrario cuando se presione el botón
                       edit = !edit;
+
+                      if (edit == false) {
+                        if (email != "") emailController.text = email;
+                        if (phoneNumber != "")
+                          phoneNumberController.text = phoneNumber;
+                        if (firstName != "")
+                          firstNameController.text = firstName;
+                        if (lastName != "") lastNameController.text = lastName;
+                      }
                     });
                   },
                   child: Text(edit
@@ -161,7 +249,34 @@ class _VolterScreenState extends State<VolterScreen> {
                 color: Colors.grey,
               ),
             ),
-            
+            if (edit) // Mostrar formularios si "edit" es verdadero
+              Column(
+                children: [
+                  buildFormField('First Name', 'Change your first name',
+                      firstNameController),
+                  buildFormField(
+                      'Last Name', 'Change your last name', lastNameController),
+                  buildFormField('Phone Number', 'Change your phone number',
+                      phoneNumberController,
+                      isNumeric: true),
+                  ElevatedButton(
+                    onPressed: () {
+                      saveChanges();
+                      setState(() {
+                        edit = !edit;
+                      });
+                    },
+                    child: Text('Save Changes'),
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.blue, // Color de fondo azul
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20.0),
+                      ),
+                    ),
+                  ),
+                ],
+              )
+            else
               ListView(
                 shrinkWrap: true,
                 children: [
@@ -200,6 +315,9 @@ class _VolterScreenState extends State<VolterScreen> {
       margin: EdgeInsets.only(bottom: 20),
       child: TextFormField(
         controller: controller,
+        onChanged: (value) {
+          controller.text = value;
+        },
         decoration: InputDecoration(
           labelText: labelText,
           hintText: hintText,
