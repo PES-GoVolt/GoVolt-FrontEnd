30becf9bfdc48bd4674c32b07dcd6e311c87ae3e
MarcChavez <castromarc2000@gmail.com>
2023-11-27 16:21:29 +0100

    better api key reference

--- govoltfrontend/lib/config.dart
+++ govoltfrontend/lib/config.dart
@@ -1,3 +1,5 @@
+import "package:govoltfrontend/api_keys.dart";
+
 class Config {
   static const String appName = "Go Volt";
   static const String apiURL = '10.0.2.2:8000'; // api de cada ordenador
@@ -23,8 +25,9 @@ class Config {
 
   //static const apiKey = "AIzaSyBisV7xClzwO5332af5dzMxo8AvZn7r4g4"; //marc
   //static const apiKey = "AIzaSyBAsdEsXKyQdZOVZYvdYCU4Vf5X6EAwMbA"; //pol
-  static const apiKey = "AIzaSyDQyP4_8RMlTgkOzV9rLIuGNB7sIcI3HLQ"; //esther
+  //static const apiKey = "AIzaSyDQyP4_8RMlTgkOzV9rLIuGNB7sIcI3HLQ"; //esther
   
+  static const apiKey = SecretKeys.apiKey;
   static const loginFIREBASE = "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=$apiKey";
   static const singupFIREBASE = "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=$apiKey";
 }
